9aa6294a003035dc3f3ca8469364e18c00374a35
tanosmaalouf <maalouf466@gmail.com>
2024-10-14 14:05:17 +0300

    EBS Completed

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello ya 3alam!'));
+app.get('/', (req, res) => res.send(`
+AWS Elastic Beanstalk Express.js Sample
+This repository contains a sample Node.js Express application designed to demonstrate how to deploy a web application on AWS Elastic Beanstalk. It includes basic setup for an Express.js server and configuration files necessary for seamless deployment to AWS's platform-as-a-service (PaaS).
+
+The project provides a template for developers looking to quickly get started with building and deploying scalable web apps using Express.js on AWS Elastic Beanstalk.`
+));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
